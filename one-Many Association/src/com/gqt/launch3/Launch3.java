c2ba7bf5e58ae5453c3a5ce11d68d366b49d1124
UR415417 <udayravsan2@gmail.com>
2023-07-21 23:37:16 +0530

    second commit

---
+++ one-Many Association/src/com/gqt/launch3/Launch3.java
@@ -0,0 +1,39 @@
+package com.gqt.launch3;
+
+import com.gqt.values2.Department;
+import com.gqt.values2.Employee;
+
+public class Launch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee emp1 = new Employee();
+		emp1.setEmpId(18);
+		emp1.setEmpName("sachin");
+		
+		Employee emp2 = new Employee();
+		emp2.setEmpId(24);
+		emp2.setEmpName("dhoni");
+		
+		Employee emp3 = new Employee();
+		emp3.setEmpId(42);
+		emp3.setEmpName("zabi");
+		
+		Employee[] e = new Employee[3];
+		e[0] = emp1;
+		e[1] = emp2;
+		e[2] = emp3;
+		
+		Department dept = new Department();
+		dept.setDeptId(1234);
+		dept.setDeptName("Bcci");
+		dept.setDeptLoc("Dubai");
+		dept.setEmployees(e);
+		
+		
+		System.out.println(dept);
+		
+	}
+
+}
---
+++ one-Many Association/src/com/gqt/values2/Department.java
@@ -0,0 +1,57 @@
+package com.gqt.values2;
+
+
+
+public class Department{
+	//instance variables
+	private int deptId;
+	private String deptName;
+	private String deptLoc;
+	
+	//Has-a variable
+    Employee[] employees;
+	
+	String data = "";
+	
+
+	
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+
+	public void setDeptLoc(String deptLoc) {
+		this.deptLoc = deptLoc;
+	}
+
+
+	public void setEmployees(Employee[] employees) {
+		this.employees = employees;
+
+	}
+	
+
+	public String toString() {
+		
+	for(Employee emp : employees) {
+		data = data + emp.getEmpId();
+		data = data + '\n'+'\n';
+		data = data + emp.getEmpName();
+		data = data + '\n'+'\n';
+	}
+		
+		return "Department details:"+'\n'+
+				deptId+'\n'+
+				deptName+'\n'+
+				deptLoc+'\n'+
+				'\n'+
+				"Employee details: "+'\n'+'\n'+data;
+
+	}
+
+}
